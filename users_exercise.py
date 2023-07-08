5d698efc070089c87a39d16a6c089d3fc5e7c279
Sean Hanlon <sean.a.hanlon@gmail.com>
2023-07-08 17:38:34 +0100

    Answer to question 8

--- users_exercise.py
+++ users_exercise.py
@@ -83,5 +83,7 @@ print(avrils_even_lotto)
 users["Erik"]["lottery_numbers"].append(7)
 print(users["Erik"]["lottery_numbers"])
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"]["home_town"] = "Edinburgh"
+print(users["Erik"]["home_town"])
 # 9. Add a pet dog to Erik called "fluffy"
 # 10. Add another person to the users dictionary
