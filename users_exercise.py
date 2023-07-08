a8fb237620158e733d3a6e5b38d84bb1682063d3
Sean Hanlon <sean.a.hanlon@gmail.com>
2023-07-08 17:39:07 +0100

    Answer to question 9

--- users_exercise.py
+++ users_exercise.py
@@ -86,4 +86,7 @@ print(users["Erik"]["lottery_numbers"])
 users["Erik"]["home_town"] = "Edinburgh"
 print(users["Erik"]["home_town"])
 # 9. Add a pet dog to Erik called "fluffy"
+users["Erik"]["pets"].append({"name": "fluffy", "species": "dog"})
+print(users["Erik"]["pets"])
 # 10. Add another person to the users dictionary
+
